2068e825d26e830e94ca83726aa88b61d6da1891
Jama Mohamed <jama3137@gmail.com>
2020-03-19 16:05:49 +0300

    [Started] Added the targetView function

--- android-animation/src/main/java/com/example/android_animation/AndroidAnimation.kt
+++ android-animation/src/main/java/com/example/android_animation/AndroidAnimation.kt
@@ -1,4 +1,18 @@
 package com.example.android_animation
 
+import android.animation.ObjectAnimator
+import android.view.View
+
 class AndroidAnimation {
+
+    private var objectAnimators = mutableListOf<ObjectAnimator>()
+    private var views = mutableListOf<View>()
+
+    fun targetView(vararg v: View) {
+        views = mutableListOf()
+        v.forEach { view ->
+            views.add(view)
+        }
+    }
+
 }
